c64a9ba4aadbe8e908701aaaca4a89329f1d6c6c
Boyoon Choi <choiboyoon11@gmail.com>
2022-01-05 21:02:37 +0100

    update 2022-01-05

--- src/server.js
+++ src/server.js
@@ -1,38 +1,22 @@
 console.log("Hello World!! Welcome to NodeJS!");
 
 import express from "express";
+import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
+
 const app = express();
-
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Now Allowed</h1>");
-  }
-  console.log("allowed. you may continue");
-  next();
-};
-
-const handleHome = (req, res) => {
-  return res.send("I love middlewares");
-};
-
-const handleProtected = (req, res) => {
-  return res.send("Welcome to the private lounge.");
-};
-
+const logger = morgan("dev");
 app.use(logger);
-app.use(privateMiddleware);
-app.get("/", handleHome);
-app.get("/protected", handleProtected);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT}🚀`);
 
-app.listen(4000, handleListening);
+app.listen(PORT, handleListening);
