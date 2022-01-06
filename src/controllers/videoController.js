f50f4181997c6f12abbe3973e7edf46841b0e106
Boyoon Choi <choiboyoon11@gmail.com>
2022-01-06 23:21:26 +0100

    add pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,12 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => {
-  return res.send(`Watch Video #${req.params.id}`);
+export const trending = (req, res) => {
+  res.render("home");
 };
-export const edit = (req, res) => res.send("Edit");
+export const see = (req, res) => {
+  return res.render("watch");
+};
+export const edit = (req, res) => {
+  return res.render("edit");
+};
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Video");
