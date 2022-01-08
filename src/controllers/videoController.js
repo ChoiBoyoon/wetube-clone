217ed6d1bce476679c3382995dd9dead35a99306
Boyoon Choi <choiboyoon11@gmail.com>
2022-01-08 23:50:54 +0100

    add video edit button

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,40 @@
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 1,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  res.render("home");
+  res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => {
-  return res.render("watch");
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", {
+    pageTitle: `Watch ${video.title}`,
+    video: video,
+  });
 };
 export const edit = (req, res) => {
   return res.render("edit");
