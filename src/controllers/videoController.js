9efb7d27e3ec6d53a47ba1ad7194a1763ee0ed4d
Boyoon Choi <choiboyoon11@gmail.com>
2022-01-09 23:09:30 +0100

    update 2022-01-09

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -36,9 +36,9 @@ export const watch = (req, res) => {
     video: video,
   });
 };
-export const edit = (req, res) => {
-  return res.render("edit");
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete Video");
+export const postEdit = (req, res) => {};
